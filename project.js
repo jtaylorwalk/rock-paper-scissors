c225ab64b7e2488bec3f565583eca39ce4f2b56c
jtaylorwalk <jtaylorwalk@gmail.com>
2024-09-12 21:02:23 -0600

    fix PlayerChoice

--- project.js
+++ project.js
@@ -6,7 +6,13 @@ let playerScore=0;
 let computerScore=0;
 function getPlayerChoice (){
     let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-   
+    if (playerChoice == "rock" || playerChoice ==  "paper" || playerChoice ==  "scissors") {
+        console.log ("You chose " + playerChoice)
+    }
+    else {
+        playerChoice = "invalid";   
+        console.log  ("Invalid. Please try again.")
+    }
     
 }
 
@@ -22,52 +28,54 @@ function getComputerChoice(){
         else { 
             computerChoice = "scissors";
         }
-    console.log ("The computer chooses " + computerChoice);
+    console.log (computerChoice);
+    
 }
 
 function round(){
-    getPlayerChoice()
-    if (playerChoice = "rock" || "paper" || "scissors") {
-        console.log ("You chose " + playerChoice)
-    }
-        getComputerChoice()
+getPlayerChoice()
+getComputerChoice()
         if (playerChoice == computerChoice) {
-        console.log ("Tie game, try again.")
+            console.log ("The computer chose " + computerChoice);
+            console.log ("Tie game, try again.")
         }
         else if (playerChoice == "rock" && computerChoice == "paper") {
-        console.log ("You lose! Paper covers rock!");
-        computerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You lose! Paper covers rock!");
+            computerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         else if (playerChoice == "rock" && computerChoice == "scissors") {
-        console.log ("You win! Rock smashes scissors!");
-        playerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You win! Rock smashes scissors!");
+            playerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         else if (playerChoice == "paper" && computerChoice == "rock") {
-        console.log ("You win! Paper covers rock!");
-        playerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You win! Paper covers rock!");
+            playerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         else if (playerChoice == "paper" && computerChoice == "scissors") {
-        console.log ("You lose! Scissors cuts paper!");
-        computerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You lose! Scissors cuts paper!");
+            computerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         else if (playerChoice == "scissors" && computerChoice == "paper") {
-        console.log ("You win! Scissors cuts paper!");
-        playerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You win! Scissors cuts paper!");
+            playerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         else if (playerChoice == "scissors" && computerChoice == "rock") {
-        console.log ("You lose! Rock smashes scissors!");
-        computerScore ++;
-        console.log ("Computer Score: " + computerScore + "Player Score: " + playerScore)
+            console.log ("The computer chose " + computerChoice);
+            console.log ("You lose! Rock smashes scissors!");
+            computerScore ++;
+            console.log ("Computer Score: " + computerScore + " Player Score: " + playerScore)
         }
         
-    else {
-            console.log ("Response invalid. Try again.")
-        }
 }
-
+round ()
 
